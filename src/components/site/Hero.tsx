9387e1b65e1b8aef9490ad47523ff313caef8bda
Younes Mekki <younesmekki@192.168.1.14>
2026-05-03 18:04:50 +0100

    Move hero content closer to navbar

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -29,7 +29,7 @@ const stats = [
 
 export const Hero = () => {
   return (
-    <section id="home" className="relative isolate min-h-[100svh] overflow-hidden bg-black pb-10 pt-24 text-white sm:pt-28 lg:pt-28">
+    <section id="home" className="relative isolate min-h-[100svh] overflow-hidden bg-black pb-10 pt-20 text-white sm:pt-[5.5rem] lg:pt-24">
       <div className="absolute inset-0 z-0 pointer-events-none">
         <img
           src="/images/mekweb-office-hero.jpg"
